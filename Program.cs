b6119105a2e268b8c9d0b64235980247f4c1ac50
Onoda Yoshiaki <y-onoda@nikki-grp.co.jp>
2019-01-14 17:38:27 +0900

    ローカルデータベース（MySQL）へ接続する

--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -28,6 +30,29 @@ namespace Master.Sync
                 Console.WriteLine(source.Count);
             }
             input.Dispose();
+
+            var builder = new MySqlConnectionStringBuilder();
+            builder.Server = ConfigurationManager.AppSettings["db:server"];
+            builder.Port = uint.Parse(ConfigurationManager.AppSettings["db:port"]);
+            builder.UserID = ConfigurationManager.AppSettings["db:user"];
+            builder.Password = ConfigurationManager.AppSettings["db:password"];
+            builder.Database = ConfigurationManager.AppSettings["db:database"];
+            builder.ConvertZeroDateTime = true;
+            builder.AllowZeroDateTime = true;
+            using (var conn = new MySqlConnection(builder.ToString()))
+            {
+                try
+                {
+                    Console.WriteLine("Connecting to MySQL...");
+                    conn.Open();
+                    // Perform database operations
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.ToString());
+                }
+            }
+            Console.WriteLine("Done.");
         }
     }
 }
